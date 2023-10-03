f692c658523f60cb405ce9ae0c2c6f2c91b323ef
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-10-03 14:03:00 +0530

    Remove unwanted options

--- Python/Image_To_Sound/main.py
+++ Python/Image_To_Sound/main.py
@@ -8,23 +8,15 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 
 # Image feeds
 img1 = cv2.imread('Capture_1.JPG')
-img2 = cv2.imread('Capture_2.JPG')
-img3 = cv2.imread('Capture_3.JPG')
 
 # ONLY FOR CHARACTER
 h1Img, w1Img, none1 = img1.shape
-h2Img, w2Img, none2 = img2.shape
-h3Img, w3Img, none3 = img3.shape
 
 # ONLY FOR CHARACTERS
 box1 = pytesseract.image_to_boxes(img1)
-box2 = pytesseract.image_to_boxes(img2)
-box3 = pytesseract.image_to_boxes(img3)
 
 # ONLY FOR WORDS
 data1 = pytesseract.image_to_data(img1)
-data2 = pytesseract.image_to_data(img2)
-data3 = pytesseract.image_to_data(img3)
 
 
 def charone():
@@ -44,46 +36,6 @@ def charone():
 
     # Output the bounding box with the image
     cv2.imshow('Image Output', img1)
-    cv2.waitKey(0)
-
-
-def chartwo():
-    for a in box2.splitlines():
-        # Converts 'box2' string into a list stored in 'a'
-        a = a.split()
-
-        # Storing values in the right variables
-        x, y = int(a[1]), int(a[2])
-        w, h = int(a[3]), int(a[4])
-
-        # Display bounding box of each letter
-        cv2.rectangle(img2, (x, h2Img - y), (w, h2Img - h), (0, 128, 0), 1)
-
-        # Display detected letter under each bounding box
-        cv2.putText(img2, a[0], (x, h2Img - y - 25), cv2.FONT_HERSHEY_PLAIN, 1, (0, 128, 0), 1)
-
-    # Output the bounding box with the imag
-    cv2.imshow('Image Output', img2)
-    cv2.waitKey(0)
-
-
-def charthree():
-    for a in box3.splitlines():
-        # Converts 'box3' string into a list stored in 'a'
-        a = a.split()
-
-        # Storing values in the right variables
-        x, y = int(a[1]), int(a[2])
-        w, h = int(a[3]), int(a[4])
-
-        # Display bounding box of each letter
-        cv2.rectangle(img3, (x, h3Img - y), (w, h3Img - h), (255, 0, 0), 1)
-
-        # Display detected letter under each bounding box
-        cv2.putText(img3, a[0], (x, h3Img - y - 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
-
-    # Output the bounding box with the image
-    cv2.imshow('Image Output', img3)
     cv2.waitKey(0)
 
 
@@ -110,58 +62,6 @@ def wordone():
 
     # Output the bounding box with the image
     cv2.imshow('Image output', img1)
-    cv2.waitKey(0)
-
-
-def wordtwo():
-    for z, a in enumerate(data2.splitlines()):
-
-        # Counter
-        if z != 0:
-
-            # Converts 'data1' string into a list stored in 'a'
-            a = a.split()
-
-            # Checking if array contains a word
-            if len(a) == 12:
-                # Storing values in the right variables
-                x, y = int(a[6]), int(a[7])
-                w, h = int(a[8]), int(a[9])
-
-                # Display bounding box of each word
-                cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 255, 0), 1)
-
-                # Display detected word under each bounding box
-                cv2.putText(img2, a[11], (x - 15, y), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 1)
-
-    # Output the bounding box with the image
-    cv2.imshow('Image output', img2)
-    cv2.waitKey(0)
-
-
-def wordthree():
-    for z, a in enumerate(data3.splitlines()):
-
-        # Counter
-        if z != 0:
-
-            # Converts 'data1' string into a list stored in 'a'
-            a = a.split()
-
-            # Checking if array contains a word
-            if len(a) == 12:
-                # Storing values in the right variables
-                x, y = int(a[6]), int(a[7])
-                w, h = int(a[8]), int(a[9])
-
-                # Display bounding box of each word
-                cv2.rectangle(img3, (x, y), (x + w, y + h), (255, 0, 0), 1)
-
-                # Display detected word under each bounding box
-                cv2.putText(img3, a[11], (x - 15, y), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
-
-    # Output the bounding box with the image
-    cv2.imshow('Image output', img3)
     cv2.waitKey(0)
 
 
@@ -210,21 +110,18 @@ def texttospeech():
 
 # Calling character methods
 while True:
-    option = input("Which option do you choose (1 - 7): ")
+    print("***********************************************")
+    print("** 1. Convert the Image to Text              **")
+    print("** 2. Output the bounding box with the image **")
+    print("** 3. Convert the Text to Speech             **")
+    print("***********************************************")
+    option = input("Which option do you choose?: ")
 
     if option == '1':
         charone()
     elif option == '2':
-        chartwo()
-    elif option == '3':
-        charthree()
-    elif option == '4':
         wordone()
-    elif option == '5':
-        wordtwo()
-    elif option == '6':
-        wordthree()
-    elif option == '7':
+    elif option == '3':
         texttospeech()
     else:
         print("Thank you for using the the OCR program")
